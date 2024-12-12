87ea392e92dfd3afa99d32bea2b1c72c78eafe55
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-12-12 13:03:32 +0400

    fix

--- game/mod_chaosbox/gamemodes/gmod_base/gamemode/shared.lua
+++ game/mod_chaosbox/gamemodes/gmod_base/gamemode/shared.lua
@@ -28,6 +28,21 @@ function FindMetaTable(name)
 	return registry[name]
 end
 
+-- TODO: Actually implement SQLite
+sql = {
+	Begin = function() end,
+	Commit = function() end,
+	IndexExists = function() end,
+	LastError = function() end,
+	Query = function() end,
+	QueryRow = function() end,
+	QueryValue = function() end,
+	SQLStr = function() end,
+	TableExists = function() end,
+}
+
+Angle = QAngle
+
 RealFrameTime = gpGlobals.absoluteframetime
 CurTime = gpGlobals.curtime
 FrameNumber = gpGlobals.framecount
@@ -35,8 +50,32 @@ FrameTime = gpGlobals.frametime
 engine.TickCount = gpGlobals.tickcount
 engine.TickInterval = gpGlobals.interval_per_tick
 
+umsg.Angle = umsg.WriteAngles
+umsg.Bool = umsg.WriteBool
+umsg.Char = umsg.WriteChar
+umsg.Entity = umsg.WriteEntity
+umsg.Float = umsg.WriteFloat
+umsg.Long = umsg.WriteLong
+umsg.Short = umsg.WriteShort
+umsg.String = umsg.WriteString
+umsg.Vector = umsg.WriteVector
+umsg.VectorNormal = umsg.WriteNormal
+umsg.Send = umsg.MessageEnd
+
+function istable(variable)
+	return type(variable) == "table"
+end
+unpack = table.unpack
+
 Material = gpGlobals.FindMaterial
 CreateMaterial = gpGlobals.CreateMaterial
+CreateConVar = function(name, value, flags, helpText, min, max)
+    if (istable(flags)) then
+        flags = bit.bor(table.unpack(flags))
+    end
+
+    return ConVar(name, value, flags, helpText, min, max)
+end
 
 --[[
 	All our libraries are plural, while with Garry's Mod they vary between plural and singular.
@@ -47,48 +86,66 @@ gamemode = require("gamemodes")
 hook = require("hooks")
 weapons = require("weapons")
 
+include("util.lua")
+include("util/sql.lua")
+
 --[[
 	Libraries taken from Garry's Mod.
 	TODO: Write own libraries.
 --]]
-matproxy = include("../modules/matproxy.lua")
-menubar = include("../modules/menubar.lua")
-notification = include("../modules/notification.lua")
-numpad = include("../modules/numpad.lua")
-player_manager = include("../modules/player_manager.lua")
--- presets = include("../modules/presets.lua") -- Not implemented atm.
-properties = include("../modules/properties.lua")
-saverestore = include("../modules/saverestore.lua")
--- scripted_ents = include("../modules/scripted_ents.lua") -- We have our own scripted_ents library.
-search = include("../modules/search.lua")
-spawnmenu = include("../modules/spawnmenu.lua")
-team = include("../modules/team.lua")
-undo = include("../modules/undo.lua")
-usermessage = include("../modules/usermessage.lua")
-utf8 = include("../modules/utf8.lua")
--- weapons = include("../modules/weapons.lua") -- We have our own weapons library.
-widget = include("../modules/widget.lua")
-ai_schedule = include("../modules/ai_schedule.lua")
-ai_task = include("../modules/ai_task.lua")
-baseclass = include("../modules/baseclass.lua")
-cleanup = include("../modules/cleanup.lua")
--- concommand = include("../modules/concommand.lua") -- We have our own concommand library.
-constraint = include("../modules/constraint.lua")
-construct = include("../modules/construct.lua")
-controlpanel = include("../modules/controlpanel.lua")
-cookie = include("../modules/cookie.lua")
-cvars = include("../modules/cvars.lua")
-draw = include("../modules/draw.lua")
-drive = include("../modules/drive.lua")
-duplicator = include("../modules/duplicator.lua")
-effects = include("../modules/effects.lua")
--- gamemode = include("../modules/gamemode.lua") -- We have our own gamemode library.
-halo = include("../modules/halo.lua")
--- hook = include("../modules/hook.lua") -- We have our own hook library.
-http = include("../modules/http.lua")
-killicon = include("../modules/killicon.lua")
-list = include("../modules/list.lua")
-markup = include("../modules/markup.lua")
+
+-- Shared modules.
+
+require("../modules/baseclass")
+-- require("../modules/concommand") -- We have our own concommand library.
+require("../modules/saverestore")
+-- require("../modules/hook") -- We have our own hook library.
+-- require("../modules/gamemode") -- We have our own gamemode library.
+-- require("../modules/weapons") -- We have our own weapons library.
+-- require("../modules/scripted_ents") -- We have our own scripted_ents library.
+require("../modules/player_manager")
+require("../modules/numpad")
+require("../modules/team")
+require("../modules/undo")
+require("../modules/cleanup")
+require("../modules/duplicator")
+require("../modules/constraint")
+require("../modules/construct")
+-- require("../modules/usermessage") -- We have our own usermessage library.
+require("../modules/list")
+require("../modules/cvars")
+require("../modules/http")
+require("../modules/properties")
+require("../modules/widget")
+require("../modules/cookie")
+require("../modules/utf8")
+
+require("../modules/drive")
+-- include("drive/drive_base.lua")
+-- include("drive/drive_noclip.lua")
+
+if (SERVER) then
+	-- Server-side modules.
+	require("../modules/ai_task")
+	require("../modules/ai_schedule")
+end
+
+if (CLIENT) then
+	-- Client-side modules.
+	require("../modules/draw")
+	-- require("../modules/markup") -- Not implemented atm.
+	require("../modules/effects")
+	require("../modules/halo")
+	require("../modules/killicon")
+	require("../modules/spawnmenu")
+	require("../modules/controlpanel")
+	-- require("../modules/presets") -- Not implemented atm.
+	require("../modules/menubar")
+	require("../modules/matproxy")
+
+	-- require("../modules/notification")
+	-- require("../modules/search")
+end
 
 --[[
 	Other shared functions.
